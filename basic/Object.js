9d4fa0879ab6f8b33303ea2270d8cf3346400fb1
Anjali Raj <97984325+anjaliraj7631@users.noreply.github.com>
2024-01-26 09:19:47 +0000

    object destructing

--- basic/Object.js
+++ basic/Object.js
@@ -139,3 +139,17 @@ class Person5 {
 const person5 = new Person5("xyz", 30, "Punjab");
 
 console.log(person5);
+
+//Object Destruting
+const book={
+    bookAuthorName:"JavaScript",
+    price:296,
+    author:"chgsdh"
+}
+
+const {bookAuthorName}=book //This syntax is a shorthand for extracting a property from an object and creating a variable with the same name.
+console.log(bookAuthorName);
+
+const {bookAuthorName:author}=book // we can destruct this object
+
+console.log(author);
