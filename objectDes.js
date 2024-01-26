cdb1870f0ccfdbbc593970c217280ce3f91a3931
Anjali Raj <97984325+anjaliraj7631@users.noreply.github.com>
2024-01-26 09:18:45 +0000

    objectDestructing

---
+++ objectDes.js
@@ -0,0 +1,13 @@
+const book={
+    bookAuthorName:"JavaScript",
+    price:296,
+    author:"chgsdh"
+}
+
+const {bookAuthorName}=book //This syntax is a shorthand for extracting a property from an object and creating a variable with the same name.
+console.log(bookAuthorName);
+
+const {bookAuthorName:author}=book // we can destruct this object
+
+console.log(author);
+
